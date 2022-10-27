a0cab33d100dad552460a8ae33fe36edb5d6a684
Cavid <hcavid386@gmail.com>
2022-10-27 22:18:16 +0400

    dsfsfsfds

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using Tools;
 
 namespace Practise
 {
+    
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            int a = 45, b = 55;
+            Calculator calculator = new Calculator();
+            Console.WriteLine(calculator.Sum(a, b));
+
         }
     }
 }
---
+++ Tools/Calculator.vb
@@ -0,0 +1,6 @@
+﻿Public Class Calculator
+
+    Public Function Sum(num1 As Integer, num2 As Integer) As Integer
+        Return num1 + num2
+    End Function
+End Class
