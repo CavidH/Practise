b28cd85a4afc6a4a8d3b190c9dff4477b70f66de
Cavid <hcavid386@gmail.com>
2022-10-29 15:13:38 +0400

    1212121212

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -3,15 +3,26 @@ using Tools;
 
 namespace Practise
 {
-    
+
     internal class Program
     {
+        int k;
+        public int k2 { get; set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            int a = 45, b = 55;
-            Calculator calculator = new Calculator();
-            Console.WriteLine(calculator.Sum(a, b));
+            Program t = new Program();
+            Console.WriteLine(t.k);
+            Console.WriteLine(t.k2);
+
+            //Tuple<int, string, bool> t = new Tuple<int, string, bool>(45, "salam", true);
+            //Console.WriteLine(t.Item3);
+
+            //Console.WriteLine(k.Length);
+            //Console.WriteLine("Hello World!");
+            //int a = 45, b = 55;
+            //Calculator calculator = new Calculator();
+            //Console.WriteLine(calculator.Sum(a, b));
 
         }
     }
